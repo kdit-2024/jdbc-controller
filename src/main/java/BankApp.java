a40454bb6c9643b72844daa2b53bcfecfb5789e2
getinthere <getinthere@naver.com>
2024-01-23 14:51:35 +0900

    리플렉션 적용

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,7 +1,13 @@
+import controller.BankController;
+import dao.BankDAO;
+
 public class BankApp {
     public static void main(String[] args) {
-        String url = "";
+        String url = "/withdraw";
 
-
+        BankDAO dao = new BankDAO();
+        BankController con = new BankController(dao);
+        Dispatcher dis = new Dispatcher(con);
+        dis.route(url);
     }
 }
